f82dacbe777ef41ed1532e0b8e4f55d2d07e79d6
tesla707 <tesla707@mail.ru>
2018-03-21 17:54:50 +0300

    Update and rename source.c to k3rnel.c

---
+++ k3rnel.c
@@ -0,0 +1,52 @@
+#include "ntdll.h"
+#include <corecrt_wstdio.h>
+
+UNICODE_STRING U_Str;
+OBJECT_ATTRIBUTES ObjAttr;
+IO_STATUS_BLOCK FileStatus;
+SECTION_IMAGE_INFORMATION SectionImageInfo;
+WIN32_FIND_DATAW FindData;
+PROCESS_BASIC_INFORMATION ProcessBasicInfo;
+SYSTEM_THREAD_INFORMATION ThreadSystemInfo;
+
+HANDLE hFile, hSection, hProcess, hThread;
+WCHAR Path[MAX_PATH];
+DWORD dwProcessId, dwThreadId;
+PVOID BaseAddress;
+SIZE_T ViewSize;
+
+void wmain(void) {
+	RtlInitUnicodeString(&U_Str, L"\\??\\E:\\MinPack\\Assassin's Creed Origins\\uplay_r1_loader64.dll"); // ?? or DosDevices ACOrigins.exe uplay_r1_loader64.dll
+	InitializeObjectAttributes(&ObjAttr, &U_Str, OBJ_KERNEL_HANDLE, 0, NULL);
+
+	NtOpenFile(&hFile, FILE_OPEN, &ObjAttr, &FileStatus, FILE_SHARE_READ, FILE_SEQUENTIAL_ONLY);
+	NtCreateSection(&hSection, MAXIMUM_ALLOWED, NULL, 0, PAGE_READONLY, SEC_IMAGE, hFile);
+	NtClose(hFile);
+	NtQuerySection(hSection, SectionImageInformation, &SectionImageInfo, sizeof(SECTION_IMAGE_INFORMATION), 0);
+	NtCreateProcess(&hProcess, MAXIMUM_ALLOWED, NULL, NtCurrentProcess(), FALSE, hSection, 0, 0);
+	NtMapViewOfSection(hSection, hProcess, &BaseAddress, 0, 0, NULL, &ViewSize, ViewShare, 0, PAGE_READONLY);
+	NtClose(hSection);
+	NtCreateThreadEx(&hThread, MAXIMUM_ALLOWED, NULL, hProcess, NULL, NULL, THREAD_CREATE_FLAGS_CREATE_SUSPENDED, 0, 0, 0, NULL);
+
+	NtQueryInformationProcess(hProcess, ProcessBasicInformation, &ProcessBasicInfo, sizeof(PROCESS_BASIC_INFORMATION), 0);
+	NtQueryInformationThread(hThread, ThreadSystemThreadInformation, &ThreadSystemInfo, sizeof(SYSTEM_THREAD_INFORMATION), 0);
+	if ((DWORD)ProcessBasicInfo.UniqueProcessId != (DWORD)ThreadSystemInfo.ClientId.UniqueProcess) {
+		NtTerminateProcess(hProcess, 0);
+		exit(-1);
+	}
+	else {
+		dwProcessId = (DWORD)ThreadSystemInfo.ClientId.UniqueProcess;
+		dwThreadId = (DWORD)ThreadSystemInfo.ClientId.UniqueThread;
+	}
+
+	K32GetModuleFileNameExW(hProcess, 0, Path, MAX_PATH);
+	FindFirstFileW(Path, &FindData);
+	
+	wprintf(L"Process name: %s\nProcess handle: %x\nProcess ID: %d\nThread handle: %x\nThread ID: %d\nEntry point: 0x%016llX\nStart address: 0x%016llX {ntdll.dll!RtlUserThreadStart}\nImage base address: 0x%016llX\nMachine: %x\nFile size: %d\nImage size: %d\n",
+		FindData.cFileName, (UINT)hProcess, dwProcessId, (UINT)hThread, dwThreadId,
+		(UINT64)SectionImageInfo.TransferAddress, ThreadSystemInfo.StartAddress, BaseAddress,
+		SectionImageInfo.Machine, SectionImageInfo.ImageFileSize, ViewSize);
+
+	getwchar();
+	NtTerminateProcess(hProcess, 0);
+}
