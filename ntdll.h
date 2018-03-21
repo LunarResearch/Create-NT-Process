99a7da3a22785fec1d2c2da1ad99d24e76501655
tesla707 <tesla707@mail.ru>
2018-03-21 17:55:22 +0300

    Update ntdll.h

--- ntdll.h
+++ ntdll.h
@@ -11,46 +11,95 @@ NTSYSCALLAPI
 NTSTATUS
 NTAPI
 NtCreateSection(
-	OUT PHANDLE SectionHandle,
-	IN ACCESS_MASK DesiredAccess,
-	IN POBJECT_ATTRIBUTES ObjectAttributes OPTIONAL,
-	IN PLARGE_INTEGER MaximumSize OPTIONAL,
-	IN ULONG SectionPageProtection,
-	IN ULONG AllocationAttributes,
-	IN HANDLE FileHandle OPTIONAL
+	_Out_ PHANDLE SectionHandle,
+	_In_ ACCESS_MASK DesiredAccess,
+	_In_opt_ POBJECT_ATTRIBUTES ObjectAttributes,
+	_In_opt_ PLARGE_INTEGER MaximumSize,
+	_In_ ULONG SectionPageProtection,
+	_In_ ULONG AllocationAttributes,
+	_In_opt_ HANDLE FileHandle
 );
 
 NTSYSCALLAPI
 NTSTATUS
 NTAPI
 NtQuerySection(
-	IN HANDLE SectionHandle,
-	IN SECTION_INFORMATION_CLASS SectionInformationClass,
-	OUT PVOID SectionInformation,
-	IN SIZE_T SectionInformationLength,
-	OUT PSIZE_T ReturnLength OPTIONAL
+	_In_ HANDLE SectionHandle,
+	_In_ SECTION_INFORMATION_CLASS SectionInformationClass,
+	_Out_writes_bytes_(SectionInformationLength) PVOID SectionInformation,
+	_In_ SIZE_T SectionInformationLength,
+	_Out_opt_ PSIZE_T ReturnLength
 );
 
 NTSYSCALLAPI
 NTSTATUS
 NTAPI
 NtCreateProcess(
-	OUT PHANDLE ProcessHandle,
-	IN ACCESS_MASK DesiredAccess,
-	IN POBJECT_ATTRIBUTES ObjectAttributes OPTIONAL,
-	IN HANDLE ParentProcess,
-	IN BOOLEAN InheritObjectTable,
-	IN HANDLE SectionHandle OPTIONAL,
-	IN HANDLE DebugPort OPTIONAL,
-	IN HANDLE ExceptionPort OPTIONAL
+	_Out_ PHANDLE ProcessHandle,
+	_In_ ACCESS_MASK DesiredAccess,
+	_In_opt_ POBJECT_ATTRIBUTES ObjectAttributes,
+	_In_ HANDLE ParentProcess,
+	_In_ BOOLEAN InheritObjectTable,
+	_In_opt_ HANDLE SectionHandle,
+	_In_opt_ HANDLE DebugPort,
+	_In_opt_ HANDLE ExceptionPort
+);
+
+NTSYSCALLAPI
+NTSTATUS
+NTAPI
+NtCreateThreadEx(
+	_Out_ PHANDLE ThreadHandle,
+	_In_ ACCESS_MASK DesiredAccess,
+	_In_opt_ POBJECT_ATTRIBUTES ObjectAttributes,
+	_In_ HANDLE ProcessHandle,
+	_In_ PVOID StartRoutine,
+	_In_opt_ PVOID Argument,
+	_In_ ULONG CreateFlags,
+	_In_ SIZE_T ZeroBits,
+	_In_ SIZE_T StackSize,
+	_In_ SIZE_T MaximumStackSize,
+	_In_opt_ PPS_ATTRIBUTE_LIST AttributeList
+);
+
+NTSYSCALLAPI
+NTSTATUS
+NTAPI
+NtMapViewOfSection(
+	_In_ HANDLE SectionHandle,
+	_In_ HANDLE ProcessHandle,
+	_Inout_ _At_(*BaseAddress, _Readable_bytes_(*ViewSize) _Writable_bytes_(*ViewSize) _Post_readable_byte_size_(*ViewSize)) PVOID *BaseAddress,
+	_In_ ULONG_PTR ZeroBits,
+	_In_ SIZE_T CommitSize,
+	_Inout_opt_ PLARGE_INTEGER SectionOffset,
+	_Inout_ PSIZE_T ViewSize,
+	_In_ SECTION_INHERIT InheritDisposition,
+	_In_ ULONG AllocationType,
+	_In_ ULONG Win32Protect
+);
+
+NTSYSCALLAPI
+NTSTATUS
+NTAPI
+NtUnmapViewOfSection(
+	_In_ HANDLE ProcessHandle,
+	_In_opt_ PVOID BaseAddress
 );
 
 NTSYSCALLAPI
 NTSTATUS
 NTAPI
 NtTerminateProcess(
-	IN HANDLE ProcessHandle OPTIONAL,
-	IN NTSTATUS ExitStatus
+	_In_opt_ HANDLE ProcessHandle,
+	_In_ NTSTATUS ExitStatus
+);
+
+NTSYSCALLAPI
+NTSTATUS
+NTAPI
+NtResumeThread(
+	_In_ HANDLE ThreadHandle,
+	_Out_opt_ PULONG PreviousSuspendCount
 );
 
 #endif // _NTDLL_H_
